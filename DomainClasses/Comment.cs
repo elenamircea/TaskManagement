daa0b0d1cda094a997dc581871e0d490f0b29e17
elenamircea <elenamircea94@gmail.com>
2016-02-21 18:12:46 +0200

    change display name

--- DomainClasses/Comment.cs
+++ DomainClasses/Comment.cs
@@ -12,7 +12,9 @@ namespace DomainClasses
         [Key]
         public int Id { get; set; }
         public String Text { get; set; }
+        [Display(Name = "Posted at")]
         public DateTime PostedAt { get; set; }
+        [Display(Name = "Updated at")]
         public DateTime UpdatedAt { get; set; }
         public String Name { get; set; }
         public int TaskId { get; set; }
--- DomainClasses/Task.cs
+++ DomainClasses/Task.cs
@@ -14,8 +14,11 @@ namespace DomainClasses
         public String Title {get; set;}
         public String Description { get; set; }
         public int Time { get; set; }
+        [Display(Name = "Asigned to")]
         public String AsignedTo { get; set; }
+        [Display(Name = "Created at")]
         public DateTime CreatedAt { get; set; }
+        [Display(Name = "Updated at")]
         public DateTime UpdatedAt { get; set; }
 
         public List<Comment> comments { get; set; }
