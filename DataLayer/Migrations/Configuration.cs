c80032c0e34dbc6278e972cc8e20e056d22b811b
elenamircea <elenamircea94@gmail.com>
2016-02-21 19:05:24 +0200

    entity framework migrations

---
+++ DataLayer/Migrations/Configuration.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity.Migrations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataLayer.Migrations
+{
+    internal sealed class Configuration : DbMigrationsConfiguration<DataLayer.EntityFrameworkContext>
+    {
+        public Configuration()
+        {
+            AutomaticMigrationsEnabled = true;
+        }
+
+        protected override void Seed(DataLayer.EntityFrameworkContext context)
+        {
+            //  This method will be called after migrating to the latest version.
+
+            //  You can use the DbSet<T>.AddOrUpdate() helper extension method 
+            //  to avoid creating duplicate seed data. E.g.
+        }
+    }
+}
